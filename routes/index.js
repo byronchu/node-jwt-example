c2336cabae0578252d60c6fb337c36796dfac2d0
Byron Chu <chu.byron@yahoo.com>
2016-12-16 14:30:54 +1100

    added special and special-header routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/special', function(req, res, next) {
+  const token = req.query.token;
+  const payload=jwt.verify(token,process.env.TOKEN_SECRET);
+  res.json({
+    emoji: "⛹",
+    email: payload.email
+  });
+});
+
+
+router.get('/special-header', function(req, res, next) {
+  const authorization= req.header('Authorization');
+  const token = authorization.split(' ')[1];
+
+
+  const payload=jwt.verify(token,process.env.TOKEN_SECRET);
+  res.json({
+    emoji: "⛹",
+    email = payload.email
+  });
+});
+
+
 module.exports = router;
